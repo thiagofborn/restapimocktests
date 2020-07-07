7cb2af2fcdd8bd6f443be1ca57ad0ea866bc718e
thborn@microsoft.com <thbor@microsoft.com>
2020-07-07 18:20:32 +0100

    First commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; } //Id will be used as the unique key in a relational DB
+        public long Shop_Id { get; set; }
+        public long Transaction_Id { get; set; }
+        public string Card_Id { get; set; }
+        public long Product_Id { get; set; }
+        public double Product_Price { get; set; }
+    }
+}
